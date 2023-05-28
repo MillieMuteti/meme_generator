7652f4ff6db415d672e8d5c1609f7e859c4aa9da
Millie Muteti <milliemuteti@gmail.com>
2023-05-28 16:51:04 +0300

    add event listener to button

--- my-app/src/components/Meme.js
+++ my-app/src/components/Meme.js
@@ -1,13 +1,25 @@
 import React from "react";
+import memesData from "../memesData"
 
 export default function Meme() {
+    function handleClick() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        console.log(url)
+        }
+        
+   
     return (
           <main>
-            <form className="form">
+            <div className="form">
                 <input type="text" placeholder="Top Text"className="form__input" />
-                <input type="text" placeholder="Bottom Textg" className="form__input"/>
-                <button className="form__button">Get a new meme image 🖼</button>
-            </form>
+                <input type="text" placeholder="Bottom Text" className="form__input"/>
+                <button className="form__button" onClick={handleClick}>
+                    Get a new meme image 🖼
+                    
+                    </button>
+            </div>
             </main>
     )
 }
