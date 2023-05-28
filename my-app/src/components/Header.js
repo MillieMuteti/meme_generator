1d64d6ceabc2a6aa3edf195fa46370d65fc1b4a7
Millie Muteti <milliemuteti@gmail.com>
2023-05-28 14:57:09 +0300

    create header component

---
+++ my-app/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header () {
+    return (
+        <nav className='navbar'>
+            <h1 className='nav__title'>MemeGenerator</h1>
+            <p className='nav__text'>React Course-Project 3</p>
+        </nav>
+    )
+}
